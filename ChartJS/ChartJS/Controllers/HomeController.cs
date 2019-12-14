34d596cbeef6237c076c9b21a82b9671425d291e
rsmilek <smilek.radim@gmail.com>
2019-12-14 10:57:35 +0100

    + Added Chart JS examples: chart.js, morris.js

---
+++ ChartJS/ChartJS/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ChartJS.Models;
+
+namespace ChartJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Random rnd = new Random();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //list of moths
+            var lstModel = new List<SimpleReportViewModel>();
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "January",
+                Quantity = rnd.Next(-100, 100)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "February",
+                Quantity = rnd.Next(-100, 100)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "March",
+                Quantity = rnd.Next(-100, 100)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "May",
+                Quantity = rnd.Next(-100, 100)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "June",
+                Quantity = rnd.Next(-100, 100)
+            });
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "July",
+                Quantity = rnd.Next(-100, 100)
+            });
+            return View(lstModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MorrisJS/MorrisJS/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MorrisJS.Models;
+
+namespace MorrisJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var lstMode = new List<SimpleReportViewModel>();
+            lstMode.Add(new SimpleReportViewModel { Year = "2008", Value = 20 });
+            lstMode.Add(new SimpleReportViewModel { Year = "2009", Value = 10 });
+            lstMode.Add(new SimpleReportViewModel { Year = "2010", Value = 5 });
+            lstMode.Add(new SimpleReportViewModel { Year = "2011", Value = 5 });
+            lstMode.Add(new SimpleReportViewModel { Year = "2012", Value = 20 });
+            return View(lstMode);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
