98ae4cf2ce44611dbe09804f94ea5c49c16ca2e3
rsmilek <smilek.radim@gmail.com>
2020-08-08 17:56:19 +0200

    +Added MVC calculator

---
+++ MVCCalculator/MVCCalculator/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCCalculator.Models;
+
+namespace MVCCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Metodou GET se odesilaji hodnoty v URL adress napr.: https://localhost:44339/Home/Index?name=Radim
+        public IActionResult Index(string name)
+        {
+            ViewBag.Name = name;
+            var calculator = new Calculator(); 
+            return View(calculator);
+        }
+
+        // Metodou POST se odesílají hodnoty z formuláře. Data se odešlou uvnitř HTTP požadavku na server.
+        [HttpPost]
+        public IActionResult Index(Calculator calculator)
+        {
+            if (ModelState.IsValid)
+            {
+                calculator.Calc();
+            }
+            return View(calculator);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCCalculator/MVCCalculator/Models/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCalculator.Models
+{
+    public class Calculator
+    {
+        [Display(Name = "Number #1")]
+        [Range(-100, 100, ErrorMessage = "Enter number in range from -100 do 100.")]
+        [Required(ErrorMessage = "Please enter valid number value!")]
+        public int Number1 { get; set; }
+        [Display(Name = "Number #2")]
+        public int Number2 { get; set; }
+        public double Result { get; set; }
+        public string Operation { get; set; }
+        public List<SelectListItem> OperationAvailable { get; set; }
+
+        public Calculator()
+        {
+            OperationAvailable = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Add", Value = "+", Selected = true },
+                new SelectListItem { Text = "Substruct", Value = "-" },
+                new SelectListItem { Text = "Multiply", Value = "*" },
+                new SelectListItem { Text = "Divide", Value = "/" }
+            };
+        }
+
+        public void Calc() 
+        {
+            switch (Operation)
+            {
+                case "+": Result = Number1 + Number2; 
+                    break;
+                case "-": Result = Number1 - Number2;
+                    break;
+                case "*": Result = Number1 * Number2;
+                    break;
+                case "/": Result = (double)Number1 / (double)Number2;
+                    break;
+            }
+
+        }
+    }
+}
